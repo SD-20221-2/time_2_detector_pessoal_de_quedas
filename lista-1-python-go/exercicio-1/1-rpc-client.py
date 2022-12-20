a8d30c9281d700ab21ff0e1910dee621c403abae
Lucas Matos <lucaspmatos@outlook.com>
2022-12-20 06:15:28 -0300

    removing unnecessary files

--- lista-1-python-go/exercicio-1/1-rpc-client.py
+++
@@ -1,8 +0,0 @@
-import xmlrpc.client
-
-with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
-    employee_name = input("Enter the employee name: ")
-    employee_position = input("Enter the employee position: ")
-    employee_salary = input("Enter the employee salary: $")
-    remote_result = proxy.employee_new_salary(employee_name, employee_position, employee_salary)
-    print(remote_result)
