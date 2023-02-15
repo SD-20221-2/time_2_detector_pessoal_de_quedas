2049963e12880346f50f486c391c9efe8ff5cd2a
getuliosilva <41757031+getuliosilva@users.noreply.github.com>
2023-02-15 10:48:42 -0300

    Update subscriber.py

--- fall-detector/subscriber.py
+++ fall-detector/subscriber.py
@@ -1,9 +1,25 @@
+#
+# Copyright 2021 HiveMQ GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import requests
+import time
 import paho.mqtt.client as paho
 from paho import mqtt
 
 MESSAGE = 'QUEDA%20DETECTADA%21%0A%0ALigar%20para%20SAMU%3A%20192'
-URL = 'https://api.callmebot.com/whatsapp.php?phone=+5562920002427&text={MESSAGE}&apikey=6594189'
+URL = 'https://api.callmebot.com/whatsapp.php?phone=+5562920002427&text=' + MESSAGE + '&apikey=6594189'
 
 def send_message():
     requests.get(URL)
@@ -11,10 +27,6 @@ def send_message():
 # setting callbacks for different events to see if it works, print the message etc.
 def on_connect(client, userdata, flags, rc, properties=None):
     print("CONNACK received with code %s." % rc)
-
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    print("mid: " + str(mid))
 
 # print which topic was subscribed to
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
@@ -34,13 +46,14 @@ client.on_connect = on_connect
 
 # enable TLS for secure connection
 client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+# set username and password
+client.username_pw_set("fall-detector", "fall-detector")
 # connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("broker.hivemq.com", 8883)
+client.connect("af4a9c8ba3224e39aad654cebd6ac673.s2.eu.hivemq.cloud", 8883)
 
 # setting callbacks, use separate functions like above for better visibility
 client.on_subscribe = on_subscribe
 client.on_message = on_message
-client.on_publish = on_publish
 
 # subscribe to all topics of encyclopedia by using the wildcard "#"
 client.subscribe("fall-detector-sd/#", qos=1)
